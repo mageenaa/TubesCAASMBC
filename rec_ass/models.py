931b3d2bcef52f93bf32e93fe260ef75de911bc3
Zaidan Mahdigit config --global user.emailzaidanici@gmail.com <zaidanici@gmail.com>
2022-12-06 21:55:35 +0700

    Updated

--- rec_ass/models.py
+++ rec_ass/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
+# Create your models here.
+class Profile(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    id_users = models.IntegerField()
+    bio = models.TextField(blank = True)
+    profileImagge = models.ImageField(upload_to='profil_images',default='Certificate(2).jpg')
+    location = models.CharField(max_length=100, blank=True)
+    
+    def _str_(self) : 
+        return self.user.username
+        
 # Create your models here.
